6c60ca4f943c929c18e985c9371b39b61e9a6d9b
sarroutbi <sarroutbi@yahoo.es>
2018-06-24 13:08:57 +0200

    Improve cmake_clean.sh

--- tfd/practice/source/cmake_clean.sh
+++ tfd/practice/source/cmake_clean.sh
@@ -1,5 +1,7 @@
 test -f Makefile && make clean
 rm -fr cmake_install.cmake
+rm -fr src/cmake_install.cmake
+rm -fr test/cmake_install.cmake
 rm -fr CMakeFiles
 rm -fr src/CMakeFiles
 rm -fr test/CMakeFiles
