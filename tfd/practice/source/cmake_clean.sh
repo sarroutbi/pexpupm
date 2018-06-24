27351de0737b5b8d52f77cd6864ed3784e3a5901
sarroutbi <sarroutbi@yahoo.es>
2018-06-24 13:07:02 +0200

    Improve cmake clean script

--- tfd/practice/source/cmake_clean.sh
+++ tfd/practice/source/cmake_clean.sh
@@ -2,6 +2,7 @@ test -f Makefile && make clean
 rm -fr cmake_install.cmake
 rm -fr CMakeFiles
 rm -fr src/CMakeFiles
+rm -fr test/CMakeFiles
 rm -fr CMakeCache*
 rm -fr Makefile
 find . -iname "*~" -print | xargs rm -fr
